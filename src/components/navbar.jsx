02d1e3ee8a2d7d24b816f464106738855afe5985
truonggiang <trantruonggiang2810@gmail.com>
2024-02-15 01:15:46 +0700

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  const links = [
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About" },
+    { url: "/project", title: "Project" },
+    { url: "contact", title: "Contact" },
+  ];
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
